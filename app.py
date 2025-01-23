5c5c510fd778960260155a580eb45b6f0d20d343
Benmoga <ben.moreno@live.com.mx>
2025-01-23 13:52:17 -0600

    Added app.py, notebooks folder, requirements.txt, and streamlit config

---
+++ app.py
@@ -0,0 +1,23 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+# Ruta del dataset
+df = pd.read_csv('C:/Users/Digitalife/Documents/Benja/CURSO Data Scientist/Proyectos/Proyecto Sprint 7/proyecto-7/.git/data/vehicles_us.csv')
+
+# Encabezado de la aplicación
+st.header('Análisis Exploratorio de Vehículos')
+
+if st.button('Generar Histograma'):
+    fig = px.histogram(df, x='price', title="Distribución de Precios de Vehículos")
+    st.write(fig)
+    st.plotly_chart(fig, key='histograma')
+
+# Botón para el gráfico de dispersión
+if st.button('Generar Gráfico de Dispersión'):
+    fig2 = px.scatter(df, x='odometer', y='price', title="Gráfico de Dispersión Odometro Vs Precio")
+    st.write(fig2)
+    st.plotly_chart(fig2, key='dispersion')
+
+        
+    
